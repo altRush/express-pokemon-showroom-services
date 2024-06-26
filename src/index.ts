d5042d0b432e6fddf98a0fe5465fdaf81e1ef571
altRush <rushing.wp@gmail.com>
2024-06-26 08:54:58 +0700

    build: Add body-parser

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express, { Application, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import routes from './routes/index';
+import bodyParser from 'body-parser';
 
 dotenv.config();
 
 const app: Application = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
 
 app.use(routes);
 
