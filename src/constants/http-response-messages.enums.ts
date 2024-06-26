95a76b23282ddaeb8ad502af51775628de859f78
altRush <rushing.wp@gmail.com>
2024-06-26 16:22:48 +0700

    chore: Add enums for http status and response message

---
+++ src/constants/http-response-messages.enums.ts
@@ -0,0 +1,8 @@
+export enum HttpResponseMessage {
+	ADD_SUCCESS = 'Pokemon is successfully added to the store',
+	ADD_FAILED = 'Failed to add the Pokemon to the store',
+	UPDATE_SUCCESS = 'Pokemon from the store is successfully updated',
+	UPDATE_FAILED = 'Failed to update the pokemon in the store',
+	GET_SUCCESS = 'Pokemon is successfully retrieved from the store',
+	GET_FAIL = 'Failed to retrieve Pokemon from store'
+}
