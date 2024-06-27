d5bfbbd668910a8f7147db9babbdb0f7dafa34ff
altRush <rushing.wp@gmail.com>
2024-06-27 14:29:51 +0700

    chore: tidy db.ts

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,9 +1,9 @@
-import pg from 'pg';
+import { Client, Pool } from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const client = new pg.Client({
+const client = new Client({
 	user: process.env.DB_USERNAME,
 	password: process.env.DB_PASSWORD,
 	host: process.env.DB_HOST,
