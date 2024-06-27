1b3f14a340e476682ba4bfdafadaa795980043ed
altRush <rushing.wp@gmail.com>
2024-06-27 16:07:49 +0700

    utils: get pokemon id from url

---
+++ src/utils/get-pokemon-id-from-url.ts
@@ -0,0 +1,10 @@
+const getPokemonIdFromUrl = (url: string) => {
+	const delimiter = '/';
+	const start = 6;
+	const tokens = url.split(delimiter).slice(start);
+	const rawResult = tokens.join(delimiter);
+	const result = rawResult.replace(/\/$/g, '');
+	return result;
+};
+
+export default getPokemonIdFromUrl;
