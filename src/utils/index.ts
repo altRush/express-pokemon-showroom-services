4e1358546e9a3a13c31fd076fdf675069393682b
altRush <rushing.wp@gmail.com>
2024-06-29 07:04:57 +0700

    chore: Re-export utils in index

---
+++ src/utils/index.ts
@@ -0,0 +1,5 @@
+import { jsArrayToSqlStringifiedArrayConverter } from './sql-array-converter';
+import client from './db';
+import getPokemonIdFromUrl from './get-pokemon-id-from-url';
+
+export { jsArrayToSqlStringifiedArrayConverter, client, getPokemonIdFromUrl };
